512d6758cd17b335e32e3b4d077bc9ad70a5d104
RositaIL <rizquierdoleguia@gmail.com>
2024-09-15 16:16:00 -0500

    Carga inicial del proyecto

---
+++ src/main/java/pe/edu/cibertec/patitasfrontendb/PatitasFrontendBApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patitasfrontendb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitasFrontendBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatitasFrontendBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/cibertec/patitasfrontendb/PatitasFrontendBApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patitasfrontendb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatitasFrontendBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
